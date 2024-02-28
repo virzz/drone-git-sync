bc7ec88ba79ac7f9f42054a45ddc343cab41a8e3
陌竹 <mozhu233@outlook.com>
2024-02-28 10:11:22 +0800

    🐞 Fix(*): create tag

--- go.mod
+++ go.mod
@@ -2,17 +2,12 @@ module github.com/virzz/drone-git-sync
 
 go 1.21.5
 
-require (
-	github.com/spf13/cobra v1.8.0
-	github.com/spf13/pflag v1.0.5
-	github.com/spf13/viper v1.18.2
-)
+require github.com/spf13/viper v1.18.2
 
 require (
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
@@ -22,6 +17,7 @@ require (
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
--- main.go
+++ main.go
@@ -1,50 +1,87 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
+	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
 	"strings"
 
-	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
-var (
-	Version string
-)
+var Version string
 
-var cmd = &cobra.Command{
-	Use:     "git-sync",
-	Short:   "git-sync plugin",
-	Version: Version,
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		cmd.Flags().VisitAll(func(f *pflag.Flag) {
-			if f.Changed {
-				viper.Set(f.Name, f.Value.String())
-			}
-		})
-	},
-	RunE: func(c *cobra.Command, args []string) error {
-		return pluginExec()
-	},
+const remoteName = "drone"
+
+func execute(cmd *exec.Cmd) error {
+	fmt.Println("+", strings.Join(cmd.Args, " "))
+	cmd.Env = os.Environ()
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	return cmd.Run()
 }
 
-func init() {
-	cmd.Flags().String("ssh-key", "", "private ssh key")
-	cmd.Flags().String("remote", "", "url of the remote repo")
-	cmd.Flags().Bool("force", false, "force push to remote")
-	cmd.Flags().Bool("skip-verify", false, "skip ssl verification")
-	cmd.Flags().Bool("debug", false, "debug mode")
-	cmd.Flags().VisitAll(func(f *pflag.Flag) {
-		viper.BindPFlag(f.Name, f)
-	})
-	viper.SetEnvPrefix("PLUGIN")
-	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
-	viper.AutomaticEnv()
+func pluginExec() error {
+	if viper.GetBool("skip-verify") {
+		cmd := exec.Command("git", "config", "--global", "http.sslVerify", "false")
+		if err := execute(cmd); err != nil {
+			return err
+		}
+	}
+	sshKey := viper.GetString("ssh-key")
+	if sshKey == "" {
+		return errors.New("missing ssh key")
+	}
+	home := "/root"
+	if currentUser, err := user.Current(); err == nil {
+		home = currentUser.HomeDir
+	}
+	sshpath := filepath.Join(home, ".ssh")
+	if err := os.MkdirAll(sshpath, 0o700); err != nil {
+		return err
+	}
+	_ = os.WriteFile(filepath.Join(sshpath, "config"), []byte("StrictHostKeyChecking no\n"), 0o700)
+	err := os.WriteFile(filepath.Join(sshpath, "id_rsa"), []byte(sshKey), 0o600)
+	if err != nil {
+		return err
+	}
+	remote := viper.GetString("remote")
+	if remote != "" {
+		cmd := exec.Command("git", "remote", "add", remoteName, remote)
+		if err := execute(cmd); err != nil {
+			return err
+		}
+	}
+	ref := ""
+	switch os.Getenv("DRONE_BUILD_EVENT") {
+	case "tag":
+		ref = os.Getenv("DRONE_TAG")
+		cmd := exec.Command("git", "tag", "-a", ref, "-m", "Ver: "+ref)
+		if err := execute(cmd); err != nil {
+			return err
+		}
+	case "push":
+		ref = os.Getenv("DRONE_BRANCH")
+	}
+	if ref == "" {
+		return errors.New("missing ref")
+	}
+	cmd := exec.Command("git", "push", remoteName, ref)
+	if viper.GetBool("force") {
+		cmd.Args = append(cmd.Args, "--force")
+	}
+	return execute(cmd)
 }
 
 func main() {
-	if err := cmd.Execute(); err != nil {
+	viper.SetEnvPrefix("PLUGIN")
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+	viper.AutomaticEnv()
+	if err := pluginExec(); err != nil {
 		log.Fatal(err)
 	}
 }
